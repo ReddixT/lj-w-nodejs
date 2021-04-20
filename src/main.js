0b766c13067bf6ebb2b5ad806067e0ae8a9710c8
Rene Ottenburg <reneottenburg@icloud.com>
2021-04-20 09:52:24 +0200

    working vue and electron

--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
-import { createApp } from "vue";
-import App from "./App";
-import mavonEditor from "mavon-editor";
-import "mavon-editor/dist/css/index.css";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp({
-  el: "#main",
-  components: { App, mavonEditor },
-  template: "<App/>",
-  data() {
-    return { value: "" };
-  },
-});
+createApp(App).mount('#app')
